4ad41e39f0c79e0e1f8ca3f994659372e26f6885
DanielSzpunar <danielszpunar@gmail.com>
2017-09-20 22:09:51 -0400

    object-oriented-programming

---
+++ 005makeObjectsPrivate.js
@@ -0,0 +1,33 @@
+var Car = function() {
+    // this is a private variable
+    var speed = 10;
+
+    // these are public methods
+    this.accelerate = function(change) {
+        speed += change;
+    };
+
+    this.decelerate = function() {
+        speed -= 5;
+    };
+
+    this.getSpeed = function() {
+        return speed;
+    };
+};
+
+var Bike = function() {
+
+    // Only change code below this line.
+    var gear = 0;
+    this.setGear = function(newGear) {
+        gear = newGear;
+    };
+    this.getGear = function() {
+        return gear;
+    };
+};
+
+var myCar = new Car();
+
+var myBike = new Bike();
